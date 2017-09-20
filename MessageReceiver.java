d5ab8558eeb7ff00eb299a9653e511560b843208
Athanasios Balanikas <athanasios.balanikas@yahoo.gr>
2017-09-21 02:49:56 +0300

    Update MessageReceiver.java

--- MessageReceiver.java
+++ MessageReceiver.java
@@ -3,8 +3,8 @@ import java.net.*;
 import java.security.*;
 public class MessageReceiver
 {	public static void main (String args[]) throws Exception
-	{	// παράλαβε από τον αποστολέα το μήνυμα και τη σύνοψή
-		// του
+	{	
+		
 		final int local_port = 7030;
 		ServerSocket receiverSocket = new ServerSocket
 (local_port);
@@ -15,15 +15,14 @@ public class MessageReceiver
 (senderSocket.getOutputStream());
 		String message = (String) in.readObject();
 		byte [] digest = (byte[]) in.readObject();
-		// δημιούργησε ένα αντικείμενο-σύνοψη
+		
 		MessageDigest md = MessageDigest.getInstance ("MD5");
-		// ενημέρωσέ το με τα προς σύνοψη δεδομένα
+		
 		md.update (message.getBytes());
-		// σύγκρινέ τη σύνοψή τους με αυτήν που έλαβες και
-		// στείλε την κατάλληλη αναφορά στον αποστολέα
+		
 		if (MessageDigest.isEqual (md.digest(), digest))
 			out.writeObject ("\nYour message was valid! ");
 		else
 			out.writeObject ("\nYour message was corrupted! ");
-	} // main
-} // MessageReceiver
+	} 
+} 
